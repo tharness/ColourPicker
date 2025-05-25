8e4e1340659f9deacc20f90f85ec36978100e72b
Trevor Harness <trevor.harness@gmail.com>
2025-05-25 11:35:11 -0400

    publishing

---
+++ publish.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+setlocal
+
+set "SOURCE=.\bin\Release\net8.0\browser-wasm\publish\wwwroot"
+set "DEST=.\docs"
+
+if not exist "%DEST%" (
+    mkdir "%DEST%"
+)
+
+xcopy "%SOURCE%\*" "%DEST%\" /E /H /Y /I
+
+endlocal
